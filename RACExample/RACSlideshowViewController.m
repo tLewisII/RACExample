2e2191483e38ef21b2e5316bd1441a923034098c
Terry Lewis <terrylewis@outlook.com>
2013-07-15 19:16:22 -0500

    Added methods to capture all the images and then add them to an array in order to continue the slideshow.

--- RACExample/RACSlideshowViewController.m
+++ RACExample/RACSlideshowViewController.m
@@ -11,6 +11,7 @@
 @interface RACSlideshowViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property(strong,nonatomic)RACSubject *subject;
+@property(strong,nonatomic)NSMutableArray *imagesArray;
 
 @end
 
@@ -18,6 +19,7 @@
 
 - (void)viewDidLoad{
     [super viewDidLoad];
+    self.imagesArray = [NSMutableArray array];
     ///We are going to load four images, each will be loaded when the preceeding one completes, giving us a slideshow effect. These are all random images from a google search for "piping plover".
     RACSignal *firstSignal = [RACSignal start:^id(BOOL *success, NSError *__autoreleasing *error) {
         NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://www.fws.gov/plover/graphics/piping_plover.jpg"]];
@@ -43,9 +45,13 @@
         NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://maineaudubon.org/wp-content/uploads/2013/04/Amanda_Reed__PipingPlover_Chick_3.jpg"]];
         return [RACSignal return:[UIImage imageWithData:data]];
     }];
-    ///Now we merge all of the images into a single signal, and then when all of them are complete, we log recognition of this to the console.
-    RACSignal *images = [[[RACSignal merge:@[firstSignalMapped,secondSignal,thirdSignal,fourthSignal]]deliverOn:[RACScheduler mainThreadScheduler]]doCompleted:^{
-        NSLog(@"Finished");
+    ///Now we merge all of the signals into a single signal, and each time a signal sends a 'next' event, we add the value to an array. When all the signals are complete. we set the imageViews animationImages property to the array of images and then start it animating after a delay. Without the delay you would not see that last image, since :doComplete would be executed immediately as it finished and thus the animation would start right away and go back to the first image.
+    RACSignal *images = [[[[RACSignal merge:@[firstSignalMapped,secondSignal,thirdSignal,fourthSignal]]deliverOn:[RACScheduler mainThreadScheduler]]doNext:^(UIImage *x) {
+        [self.imagesArray addObject:x];
+    }] doCompleted:^{
+        self.imageView.animationImages = self.imagesArray;
+        self.imageView.animationDuration = 4;
+        [self.imageView performSelector:@selector(startAnimating) withObject:nil afterDelay:2];
     }];
     
     RAC(self.imageView.image) = images;
