17b96ad740c488bf337818be95b8912d0925618d
Terry Lewis <terrylewis@outlook.com>
2014-01-18 12:34:59 -0600

    Added viewController for Photo Library example.

---
+++ RACExample/RACPhotosViewController.h
@@ -0,0 +1,13 @@
+//
+//  RACPhotosViewController.h
+//  RACExample
+//
+//  Created by Terry Lewis II on 1/18/14.
+//  Copyright (c) 2014 Terry Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RACPhotosViewController : UIViewController
+
+@end
---
+++ RACExample/RACPhotosViewController.m
@@ -0,0 +1,24 @@
+//
+//  RACPhotosViewController.m
+//  RACExample
+//
+//  Created by Terry Lewis II on 1/18/14.
+//  Copyright (c) 2014 Terry Lewis. All rights reserved.
+//
+
+#import "RACPhotosViewController.h"
+
+@interface RACPhotosViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@end
+
+@implementation RACPhotosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+
+@end
