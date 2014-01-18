959514ef182a858c74516a549fd74c1277f70d62
Terry Lewis <terrylewis@outlook.com>
2014-01-18 12:51:26 -0600

    Removed un-necessary tableView datasource class, added initial albums implementation.

--- RACExample/RACPhotosViewController.m
+++ RACExample/RACPhotosViewController.m
@@ -7,16 +7,33 @@
 //
 
 #import "RACPhotosViewController.h"
-
+#import "RACPhotoManager.h"
+#import "TLDataSource.h"
 @interface RACPhotosViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) TLDataSource *datasource;
+@property (strong, nonatomic) RACPhotoManager *photoManager;
 @end
 
 @implementation RACPhotosViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    self.photoManager = [RACPhotoManager new];
+    __strong typeof (UITableView *) strongTableView = self.tableView;
+    RAC(self, datasource) = [[[[self.photoManager photoAlbums]map:^id(NSArray *value) {
+        CellConfigureBlock block = ^(UITableViewCell *cell, ALAssetsGroup *result, id indexPath) {
+            cell.textLabel.text = [result valueForProperty:ALAssetsGroupPropertyName];
+            cell.imageView.image = [UIImage imageWithCGImage:result.posterImage];
+        };
+        
+        TLDataSource *dataSource = [[TLDataSource alloc]initWithItems:value cellIdentifier:@"CELL" configureCellBlock:block];
+        return dataSource;
+    }]catchTo:[RACSignal empty]]doCompleted:^{
+        [strongTableView reloadData];
+    }];
+    
+    RAC(self.tableView, dataSource, nil) = RACObserve(self, datasource);
 }
 
 
