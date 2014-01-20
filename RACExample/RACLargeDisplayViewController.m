21a2de2a2b0729544743f2b5657936cdf0eafd69
Terry Lewis <terrylewis@outlook.com>
2014-01-20 08:39:32 -0600

    Added classes for large photo display.

---
+++ RACExample/RACLargeDisplayViewController.m
@@ -0,0 +1,38 @@
+//
+//  RACLargeDisplayViewController.m
+//  RACExample
+//
+//  Created by Terry Lewis II on 1/20/14.
+//  Copyright (c) 2014 Terry Lewis. All rights reserved.
+//
+
+#import "RACLargeDisplayViewController.h"
+
+@interface RACLargeDisplayViewController ()
+
+@end
+
+@implementation RACLargeDisplayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
