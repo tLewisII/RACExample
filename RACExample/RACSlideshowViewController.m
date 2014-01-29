a691e81f2b399d6c1ec4d3c52efafa8d96fb7df0
Terry Lewis <terrylewis@outlook.com>
2014-01-28 18:23:26 -0600

    Refactored slideshow example.

--- RACExample/RACSlideshowViewController.m
+++ RACExample/RACSlideshowViewController.m
@@ -9,54 +9,65 @@
 #import "RACSlideshowViewController.h"
 
 @interface RACSlideshowViewController ()
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property(weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation RACSlideshowViewController
 
-- (void)viewDidLoad{
+- (void)viewDidLoad {
     [super viewDidLoad];
-    ///We are going to load four images, each will be loaded when the preceeding one completes, giving us a slideshow effect. These are all random images from a google search for "piping plover".
-    RACSignal *firstSignal = [RACSignal startEagerlyWithScheduler:[RACScheduler schedulerWithPriority:RACSchedulerPriorityBackground] block:^(id<RACSubscriber> subscriber) {
-        [[UIApplication sharedApplication]setNetworkActivityIndicatorVisible:YES];
-        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://www.fws.gov/plover/graphics/piping_plover.jpg"]];
-         [subscriber sendNext:[UIImage imageWithData:data]];
-        [subscriber sendCompleted];
+    NSURL *firstURL = [NSURL URLWithString:@"http://www.fws.gov/plover/graphics/piping_plover.jpg"];
+    NSURL *secondURL = [NSURL URLWithString:@"http://www.jkcassady.com/images/1PIPL707.jpg"];
+    NSURL *thirdURL = [NSURL URLWithString:@"http://m4.i.pbase.com/o6/21/832521/1/126531614.sfZPR2Eb.PipingPloverBaby.jpg"];
+    NSURL *fourthURL = [NSURL URLWithString:@"http://maineaudubon.org/wp-content/uploads/2013/04/Amanda_Reed__PipingPlover_Chick_3.jpg"];
+
+    ///We are going to load four images, each will be loaded when the preceding one completes, giving us a slideshow effect. These are all random images from a google search for "piping plover".
+    [[UIApplication sharedApplication]setNetworkActivityIndicatorVisible:YES];
+    RACSignal *firstSignal = [[[[RACSignal return:firstURL]deliverOn:[RACScheduler schedulerWithPriority:RACSchedulerPriorityBackground]]tryMap:^id(id value, NSError **errorPtr) {
+        return [NSData dataWithContentsOfURL:value options:0 error:errorPtr];
+    }]delay:.227];
+
+    RACSignal *secondSignal = [[firstSignal then:^RACSignal * {
+        return [[RACSignal return:secondURL]tryMap:^id(id value, NSError **errorPtr) {
+            return [NSData dataWithContentsOfURL:value options:0 error:errorPtr];
+        }];
+    }]delay:1];
+
+    RACSignal *thirdSignal = [[secondSignal then:^RACSignal * {
+        return [[RACSignal return:thirdURL]tryMap:^id(id value, NSError **errorPtr) {
+            return [NSData dataWithContentsOfURL:value options:0 error:errorPtr];
+        }];
+    }]delay:1];
+
+    RACSignal *fourthSignal = [[thirdSignal then:^RACSignal * {
+        return [[RACSignal return:fourthURL]tryMap:^id(id value, NSError **errorPtr) {
+            return [NSData dataWithContentsOfURL:value options:0 error:errorPtr];
+        }];
+    }]delay:1];
+
+    ///Now we merge all of the signals into a single signal, and then call `-replay` in order to make sure that the
+    ///images are available when we call `-images.collect` in order to start the slideshow.
+    ///This ensures that we don't hit the network again once all the request complete. Any errors sent by any of the signals will also be caught here.
+    RACSignal *imageData = [[[[RACSignal merge:@[firstSignal, secondSignal, thirdSignal, fourthSignal]]deliverOn:[RACScheduler mainThreadScheduler]]replay]catchTo:[RACSignal empty]];
+
+    /// We map the `imageData` signal and convert all of the data to UIImage's.
+    RACSignal *imagesFromData = [imageData map:^id(NSData *value) {
+        return [UIImage imageWithData:value];
     }];
 
-    RACSignal *firstSignalMapped = [firstSignal catch:^RACSignal *(NSError *error) {
-        return [RACSignal empty];
-    }];
-    
-    RACSignal *secondSignal = [firstSignalMapped then:^RACSignal *{
-        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://www.jkcassady.com/images/1PIPL707.jpg"]];
-        return [RACSignal return:[UIImage imageWithData:data]];
-    }];
-    
-    RACSignal *thirdSignal = [secondSignal then:^RACSignal *{
-        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://m4.i.pbase.com/o6/21/832521/1/126531614.sfZPR2Eb.PipingPloverBaby.jpg"]];
-        return [RACSignal return:[UIImage imageWithData:data]];
-    }];
-    
-    RACSignal *fourthSignal = [thirdSignal then:^RACSignal *{
-        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://maineaudubon.org/wp-content/uploads/2013/04/Amanda_Reed__PipingPlover_Chick_3.jpg"]];
-        return [RACSignal return:[UIImage imageWithData:data]];
-    }];
-    ///Now we merge all of the signals into a single signal, and then call `-replay` in order to make sure that the images are available when we call `-images.collect` in order to start the slideshow. This ensures that we don't hit the network again once all the request complete.
-    RACSignal *images = [[[RACSignal merge:@[firstSignalMapped,secondSignal,thirdSignal,fourthSignal]]deliverOn:[RACScheduler mainThreadScheduler]]replay];
-    
-    RAC(self.imageView,image) = images;
+    RAC(self.imageView, image) = imagesFromData;
     @weakify(self)
     ///When the signal completes we want to gather all of the images into an array and start the slideshow over again.
-    RAC(self.imageView,animationImages) = [images.collect doCompleted:^{
+    RAC(self.imageView, animationImages) = [imagesFromData.collect doCompleted:^{
         @strongify(self)
         [[UIApplication sharedApplication]setNetworkActivityIndicatorVisible:NO];
         self.imageView.animationDuration = 4;
         [self.imageView performSelector:@selector(startAnimating) withObject:nil afterDelay:1.5];
     }];
 }
--(void)viewDidDisappear:(BOOL)animated {
+
+- (void)viewDidDisappear:(BOOL)animated {
     [[UIApplication sharedApplication]setNetworkActivityIndicatorVisible:NO];
 }
 @end
