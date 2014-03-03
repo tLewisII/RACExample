cf3dc420105c3965aae4d8b1064cf6d1f0163637
Terry Lewis <terrylewis@outlook.com>
2014-03-02 21:08:45 -0600

    Small formatting change

--- RACExample/TLViewController.m
+++ RACExample/TLViewController.m
@@ -8,6 +8,7 @@
 
 #import "TLViewController.h"
 #import "TLDataSource.h"
+
 @interface TLViewController () <UITableViewDelegate>
 @property(strong, nonatomic) UITableView *tableView;
 @property(strong, nonatomic) TLDataSource *datasource;
@@ -19,22 +20,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"RACExample";
-    self.tableView = [[UITableView alloc]initWithFrame:self.view.bounds];
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
     CellConfigureBlock block = ^(UITableViewCell *cell, NSString *item, id indexPath) {
         cell.textLabel.text = item;
     };
-    self.datasource = [[TLDataSource alloc]initWithItems:@[@"Imperative Gestures", @"Gestures", @"Imperative Text Fields", @"Text Fields", @"Slideshow", @"Photo Library"]
-                                          cellIdentifier:@"Cell"
-                                      configureCellBlock:block];
+    self.datasource = [[TLDataSource alloc] initWithItems:@[@"Imperative Gestures",
+                                                            @"Gestures",
+                                                            @"Imperative Text Fields",
+                                                            @"Text Fields",
+                                                            @"Slideshow",
+                                                            @"Photo Library"]
+            
+                                           cellIdentifier:@"Cell"
+                                       configureCellBlock:block];
     self.tableView.dataSource = self.datasource;
     self.tableView.delegate = self;
     [self.view addSubview:self.tableView];
-    
+
     ///ReactiveCocoa
     self.delegateSubject = [RACSubject subject]; ///RACSubjects are useful for bridging the gap between the normal world of Objective-C and ReactiveCocoa.
     ///We lift the selector into the RAC world, and it will be invoked every time the delegate subject sends a new value.
-    [self rac_liftSelector:@selector(performSegueWithIdentifier:sender:) withSignals:self.delegateSubject,[RACSignal return:nil], nil];
+    [self rac_liftSelector:@selector(performSegueWithIdentifier:sender:) withSignals:self.delegateSubject, [RACSignal return:nil], nil];
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
